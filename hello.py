53e2f5e6932cfbf5ce95dbc8830bb878b2c10553
angelrgo <angelrgo@gmail.com>
2023-01-03 22:29:39 +0100

    Primer programa en Python

---
+++ hello.py
@@ -0,0 +1,8 @@
+#Hola Mundo, esto es un comentario
+print("Hola Python")
+print('Hola Python con comilla simple')
+""" Podemos hacer comentarios
+en varias líneas"""
+''' Este tambien
+es un comentario multilínea'''
+
